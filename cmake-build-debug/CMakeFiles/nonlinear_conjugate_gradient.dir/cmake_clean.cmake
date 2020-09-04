f92b6341c7df35e31d397228814a24d8589d57b9
Alireza <alireza.lm69@gmail.com>
2020-09-03 21:28:40 -0300

    Project Init

---
+++ cmake-build-debug/CMakeFiles/nonlinear_conjugate_gradient.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/nonlinear_conjugate_gradient.dir/main.cpp.o"
+  "nonlinear_conjugate_gradient"
+  "nonlinear_conjugate_gradient.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/nonlinear_conjugate_gradient.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
